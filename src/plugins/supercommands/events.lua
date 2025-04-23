34655decb20f41409a75fe997def1853630c99a8
criskkky <criskywww@gmail.com>
2025-04-22 23:20:53 -0400

    refactor(src): all events

--- src/plugins/supercommands/events.lua
+++ src/plugins/supercommands/events.lua
@@ -1,27 +1,27 @@
-AddEventHandler("OnPlayerDamage", function(event, playerid)
-	local player = GetPlayer(playerid)
-	if not player then return end
-	if player:GetVar("godmode") then return event:SetReturn(false) end
+AddEventHandler("OnPlayerDamage", function(p_Event, p_PlayerID)
+	local l_Player = GetPlayer(p_PlayerID)
+	if not l_Player then return end
+	if l_Player:GetVar("godmode") then return p_Event:SetReturn(false) end
 	return EventResult.Continue
 end)
 
-AddEventHandler("OnPlayerSpawn", function(event)
-	local playerid = event:GetInt("userid")
-	local player = GetPlayer(playerid)
-	if not player then return end
-	if player:GetVar("godmode") then return player:SetVar("godmode", false) end
+AddEventHandler("OnPlayerSpawn", function(p_Event)
+	local l_PlayerID = p_Event:GetInt("userid")
+	local l_Player = GetPlayer(l_PlayerID)
+	if not l_Player then return end
+	if l_Player:GetVar("godmode") then return l_Player:SetVar("godmode", false) end
 	return EventResult.Continue
 end)
 
-AddEventHandler("OnPostPlayerDeath", function(event)
-	local playerid = event:GetInt("userid")
-	local player = GetPlayer(playerid)
-	if not player then return end
-	if player then
-		local pos = player:CBaseEntity().CBodyComponent.SceneNode.AbsOrigin
-		local rot = player:CBaseEntity().CBodyComponent.SceneNode.AbsRotation
-		player:SetVar("death_pos", tostring(pos))
-		player:SetVar("death_rot", tostring(rot))
+AddEventHandler("OnPostPlayerDeath", function(p_Event)
+	local l_PlayerID = p_Event:GetInt("userid")
+	local l_Player = GetPlayer(l_PlayerID)
+	if not l_Player then return end
+	if l_Player then
+		local l_Pos = l_Player:CBaseEntity().CBodyComponent.SceneNode.AbsOrigin
+		local l_Rot = l_Player:CBaseEntity().CBodyComponent.SceneNode.AbsRotation
+		l_Player:SetVar("death_pos", tostring(l_Pos))
+		l_Player:SetVar("death_rot", tostring(l_Rot))
 	end
 	return EventResult.Continue
 end)
